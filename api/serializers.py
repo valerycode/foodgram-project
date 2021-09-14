88d6b2afe495565d476077ddab3ead3b59298f94
valerycode <lera200@yandex.ru>
2021-09-14 14:53:44 +0300

    Delete extra blank line

--- api/serializers.py
+++ api/serializers.py
@@ -1,5 +1,5 @@
 from django.contrib.auth import get_user_model
-
+from django.core.exceptions import ValidationError
 from rest_framework import serializers
 
 from recipes.models import Ingredient, Recipe
