59075cd7f7eb98fd8b33d0f44699edb5b375c36e
valerycode <lera200@yandex.ru>
2021-10-04 15:57:45 +0300

    Change CreationForm

--- users/forms.py
+++ users/forms.py
@@ -7,4 +7,4 @@ User = get_user_model()
 class CreationForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = User
-        fields = ('username', 'first_name', 'last_name', 'email', 'password')
+        fields = ('username', 'first_name', 'last_name', 'email')
